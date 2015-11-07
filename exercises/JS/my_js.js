b44643a6ce67e421f928e37482a08ee697bda9b7
Tito Esteves <titoesteves@live.com>
2015-11-07 01:49:14 -0500

    dot, dotcall, new, and instanceof functions completed

--- exercises/JS/my_js.js
+++ exercises/JS/my_js.js
@@ -1,9 +1,10 @@
 // 05 Context
 DOT = function(obj, prop) {
-  if (obj.hasOwnProperty(prop)) {
-    return obj[prop];
-  } else if (obj.__proto__) {
-    return DOT(obj.__proto__, prop);
+  if(obj.hasOwnProperty(prop)){
+  	return obj[prop];
+  }
+  else if(obj.__proto__){
+  	return DOT(obj.__proto__, prop);
   }
   return undefined;
 };
@@ -11,16 +12,28 @@ DOT = function(obj, prop) {
 // 05 Context
 DOTCALL = function(obj, prop, args) {
   var fn = DOT(obj, prop);
-  if(fn) {
-    return fn.apply(obj, args);
+  if(fn){
+  	return fn.apply(obj, args);
   }
 };
 
 // 06 Prototypes
 NEW = function(constructor, args) {
-  
+	var obj = {};
+	obj.__proto__ = constructor.prototype;
+	constructor.apply(obj, args);
+	return obj;
 };
 
 INSTANCEOF = function(obj, constructor) {
-
+  // 
+  if(obj.__proto__ === constructor.prototype){
+    return true;
+  }
+  else if(obj.__proto__){
+    return INSTANCEOF(obj.__proto__, constructor);
+  }
+  else {
+    return false;
+  }
 };
